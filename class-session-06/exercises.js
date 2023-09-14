62b62dcfce438472bf8ce2b2ca1c21be63ff6be2
hilindst <114443718+hilindst@users.noreply.github.com>
2023-09-13 20:23:57 -0500

    Update exercises.js

--- class-session-06/exercises.js
+++ class-session-06/exercises.js
@@ -72,27 +72,29 @@ console.log(evan.info());//"not a function" because static affects Class not ins
 
 
 //Exercise 5 
-class Person {
+/* class Person {
   constructor(name, age) {
-    this.name = name;
-    this.age = age;
+    this._name = name;
+    this._age = age;
   }
-  getAge(){
-    return this.age;
-  }
-
-  setAge(newAge){
-    if (newAge < 0){
-      return 'Age cannot be less than 0';
-    } else
-    this.age = newAge;
-  }
-
   static info(){
     return "This is a Person class.";
   }
+  get age() {
+    return this._age;
+  }
+
+  set age(value) {
+    if (value < 0){
+      console.log('Age cannot be less than 0');
+    } else {
+    this._age = value;
+    }
+  }
+
+  
   happyBirthday(){
-    console.log(`Happy Birthday, ${this.name}! You are ${this.age} years old!`)
+    console.log(`Happy Birthday, ${this._name}! You are ${this._age} years old!`)
   }
 }
 
@@ -103,6 +105,48 @@ const hilary = new Person('Hilary', 37);
 julia.happyBirthday();
 console.log(Person.info());//"this is a person class"!! yay I did it
 console.log(evan.info()); */
-hilary.happyBirthday();
-hilary.setAge(-7);
-hilary.happyBirthday();
+/* hilary.age=-6;//console log message, then runs original input in const...
+hilary.age=60;
+hilary.happyBirthday(); */
+
+//Exercise 6 in Private Fields in Classes repository
+
+
+//Exercise 7  definitely need more insight. Would I use methods like for an array?
+class Library {
+  constructor(book){
+  book(ISBN, title, author, yearPublished) {
+      this.#ISBN = ISBN;
+      this.title = title;
+      this.author = author;
+      this.yearPublished = yearPublished;
+    }
+  }
+
+  get book() {
+    return this.title + this.author + this.yearPublished;
+  }
+
+  set addBook(book){
+    [this.title, this.author, this.yearPublished] = book;
+  }
+
+  removeBook(bookTitle){
+    for (let i = 0; i < this.books.length; index++){
+      const book = this.books[i];
+      if(book.title == bookTitle){
+        this.books.splice(i);
+      }
+    }
+  }
+
+  listBooks(){
+    for (const book of this.books){
+      console.log(`${book.title} by ${book.author} published in ${book.yearPublished}`)
+    }
+  }
+}
+
+
+const book1 = new Book(1234, "The Dreaming Tree", "C.J. Cherryh", 1997);
+const book2 = new Book(2345, "Catch-22", "Joseph Heller", 1961);
